d2276ad92a40b51d8a0e1263727144b0171dc424
Benjamin <bavogel@iastate.edu>
2019-10-29 14:50:27 -0500

    Began work on crawl() method and made Vector() class

--- src/pa1/Crawler.java
+++ src/pa1/Crawler.java
@@ -1,15 +1,29 @@
 package pa1;
 
 import api.Graph;
+import api.Util;
+
+import java.util.ArrayList;
+import java.util.Queue;
+
+import org.jsoup.Jsoup;
+import org.jsoup.UnsupportedMimeTypeException;
+import org.jsoup.HttpStatusException;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  * Implementation of a basic web crawler that creates a graph of some
  * portion of the world wide web.
  *
- * @author PLEASE FILL IN TEAM MEMBER NAMES HERE
+ * @author Benjamin Vogel
  */
 public class Crawler
 {
+  private String seed = "";
+  private int maximumDepth = 0;
+  private int maximumPages = 0;
   /**
    * Constructs a Crawler that will start with the given seed url, including
    * only up to maxPages pages at distance up to maxDepth from the seed url.
@@ -19,7 +33,10 @@ public class Crawler
    */
   public Crawler(String seedUrl, int maxDepth, int maxPages)
   {
-    // TODO
+    seed = seedUrl;
+    maximumDepth = maxDepth;
+    maximumPages = maxPages;
+    // TODO: Make a graph here or just make one and return it in the crawl() method?
   }
   
   /**
@@ -31,7 +48,71 @@ public class Crawler
    */
   public Graph<String> crawl()
   {
-    // TODO
+    // Initialize the discovered arraylist to keep track of the pages crawled
+    // TODO: Change discovered array list to be of integers and have the currentPages be the index
+    ArrayList<Vertex> discovered = new ArrayList<Vertex>();
+    Queue<Vertex> Q = new Queue();
+
+    Vertex seedVertex = new Vertex(seed, 0);
+    Q.add(seedVertex);
+    discovered.add(seedVertex);
+
+    int currentDepth = 0;
+    int currentPages = 1;
+
+    while (!Q.isEmpty())
+    {
+      Vertex currentVertex = Q.remove();
+
+      Document doc = Jsoup.connect(currentVertex.getUrl()).get();
+
+      Element links = doc.select("a[href]");
+      for (Element link : links)
+      {
+        String v = link.attr("abs:href");
+
+        Document temp = null;
+        if (!Util.ignoreLink(currentVertex.getUrl(), v))
+        {
+          try
+          {
+            // TODO: Implement a politeness policy
+            temp = Jsoup.connect(v).get();
+          }
+          catch (UnsupportedMimeTypeException e)
+          {
+            System.out.println("--unsupported document type, do nothing");
+            continue;
+          } 
+          catch (HttpStatusException  e)
+          {
+            System.out.println("--invalid link, do nothing");
+            continue;
+          }
+
+          Vertex newVertex = new Vertex(v);
+
+          if (!discovered.contains(newVertex))
+          {
+            int newVertexDepth = currentVertex.getDepth() + 1;
+            if (currentPages < maximumPages && newVertexDepth <= maximumDepth)
+            {
+              // Set the new vertex's depth and in degree
+              newVertex.setDepth(newVertexDepth);
+
+              discovered.add(newVertex);
+              Q.add(newVertex);
+              currentPages++;
+            }
+          }
+        }
+        else
+        {
+          System.out.println("--ignore");
+          continue;
+        }
+      }
+    }
     return null;
   }
 }
---
+++ src/pa1/Vertex.java
@@ -0,0 +1,41 @@
+package pa1;
+
+class Vertex
+{
+    private String url;
+    private int depth;
+    private ArrayList<Vertex> ancestors;
+    private ArrayList<Vertex> children;
+
+    public Vertex(String initUrl, int initDepth)
+    {
+        url = initUrl;
+        depth = initDepth;
+    }
+
+    public Vertex(String initUrl)
+    {
+        url = initUrl;
+        depth = 0;
+    }
+
+    public String getUrl() { return url; }
+
+    public void setUrl(String url) { this.url = url; }
+
+    public int getDepth() { return depth; }
+
+    public void setDepth(int depth) { this.depth = depth; }
+
+    
+    public boolean equals(Object object) {
+        if (this == object) return true;
+
+        if (object == null || getClass() != object.getClass()) return false;
+
+        if (!super.equals(object)) return false;
+
+        Vertex vertex = (Vertex) object;
+        return getUrl().equals(vertex.getUrl());
+    }
+}
